e0124d6326b82134c6cca75cd84e5116553d871e
Cole Haus <colehaus@cryptolab.net>
2014-10-27 17:10:25 -0400

    Basic HTML5 <menu> polyfill

---
+++ menu.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var menuContents = function(el) {
+    var menuItem = function(el_) {
+        var el = $(el_);
+        var lb = el.attr('label');
+        if (lb !== null) {
+            var li = $('<li/>').text(lb).attr('onclick', el.attr('onclick'));
+            li.click(function(ev) {
+                $(ev.target).parent().remove();
+            });
+            return [li];
+        } else {
+            return [];
+        }
+    };
+    var menu = function(el_) {
+        var el = $(el_);
+        var lb = el.attr('label');
+        if (lb !== null) {
+            return [$('<li>'+lb+'</li>').get()];
+        } else {
+            return [$('<hr/>').get()].concat(menuContents(el), [$('<hr/>').get()]);
+        }
+    };
+
+    var contents = $(el).children('menuitem, hr, menu').map(function(_, el) {
+        switch (el.nodeName) {
+            case 'MENUITEM':
+              return menuItem(el);
+              break;
+            case 'HR':
+              return [el];
+              break;
+            case 'MENU':
+              return menu(el);
+              break;
+        }
+    });
+    var flattened = Array.prototype.concat.apply([], contents);
+    var l = flattened.length;
+    var finalContents = flattened.reduce(function(pv, cv, i) {
+        var onEnd = i === 0 || i === (l - 1);
+        var prevHr = typeof pv[i-1] !== 'undefined' && pv[i-1].nodeName === 'HR';
+        var isEmpty = cv.nodeName === 'LI' && $(cv).text() === '';
+        if ((onEnd || prevHr) && cv.nodeName === 'HR' || isEmpty) {
+            return pv;
+        } else {
+            pv.push(cv);
+            return pv;
+        }
+    }, []);
+    return $('<ul class="menu"></ul>').append(finalContents);
+};
+
+var toggleMenu = function(ev) {
+    var isPopup = function(_) {
+        var el = $(this);
+        var pe = el.parent().get(0);
+
+        var isMenu = this.nodeName === 'MENU';
+        var isSelfPop = el.attr('type') === 'popup';
+        var hasParentPop = typeof pe !== 'undefined' && isPopup(pe);
+
+        return isMenu && (isSelfPop || hasParentPop);
+    };
+
+    var el = ev.target;
+    var mn = $('#'+$(el).attr('menu')).filter(isPopup);
+    var ul = mn.children('ul.menu');
+    if (ul.length === 0) {
+       mn.append(menuContents(mn));
+    } else {
+       ul.remove();
+    }
+};
+
+$(function () {
+    $('[type="menu"]').each(function(_, el) {
+        $(el).click(toggleMenu);
+    });
+});
