09ce5439bd63e202729e68653447e11a84d2a583
Cole Haus <colehaus@cryptolab.net>
2015-02-28 05:06:40 -0500

    Switch to scss entirely. Uglify js.

--- posts/quorum/warnings.md
+++ posts/quorum/warnings.md
@@ -1,3 +1,2 @@
-False assumptions|The model relies on several assumptions which are not likely borne out
-Out of my depth|I am not a statistical wunderkind
+False assumptions|The model relies on several assumptions which are probably not true
 Technocratic|Dialed to 11
--- site.hs
+++ site.hs
@@ -15,82 +15,95 @@ main = hakyll $ do
   tags <- buildTags postPat (fromCapture "posts/tag/*/index.html")
   tagsRules tags $ \tag pat -> do
     route idRoute
-    compile $ let ctx = constField "title" ("Tagged " <> tag) <>
-                        constField (tag <> "-page") mempty <>
-                        listField "posts"
-                                  (tagsCtx tags <> postCtx)
-                                  (recentFirst =<< loadAll pat) <>
-                                  defaultContext in
-              makeItem mempty >>=
-              loadAndApplyTemplate "templates/tag.html" ctx >>=
-              finish ctx
+    compile $ let
+      ctx =
+        constField "title" ("Tagged " <> tag) <>
+        constField (tag <> "-page") mempty <>
+        listField "posts"
+                  (tagsCtx tags <> postCtx)
+                  (recentFirst =<< loadAll pat) <>
+                  defaultContext in
+      makeItem mempty >>=
+      loadAndApplyTemplate "templates/tag.html" ctx >>=
+      finish ctx
   create ["tags/index.html"] $ do
     route idRoute
     compile $ do
       tagCloud <- renderTagCloud 50 180 tags
-      let ctx = constField "tag-cloud" tagCloud <>
-                constField "title" "Tags" <>
-                constField "tags-page" mempty <>
-                defaultContext
+      let ctx =
+            constField "tag-cloud" tagCloud <>
+            constField "title" "Tags" <>
+            constField "tags-page" mempty <>
+            defaultContext
       makeItem mempty >>=
         loadAndApplyTemplate "templates/tags.html" ctx >>=
         finish ctx
 
-  pages <- buildPaginateWith ((paginateOverflow perPage <$>) . sortChronological)
-                            postPat
-                            (fromCapture (fromGlob "posts/page/*/index.html") . show)
+  pages <- buildPaginateWith
+           ((paginateOverflow perPage <$>) . sortChronological)
+           postPat
+           (fromCapture (fromGlob "posts/page/*/index.html") . show)
   paginateRules pages $ \n pat -> do
     route idRoute
-    compile $ let pageCtx = paginateContext pages n
-                  ctx = constField "title" ("Page " <> show n) <>
-                        listField "posts"
-                                  (teaserField "teaser" "teaser" <>
-                                   tagsCtx tags <>
-                                   pageCtx <>
-                                   postCtx)
-                                  (recentFirst =<< loadAll pat) <>
-                        pageCtx <>
-                        defaultContext in
-              makeItem mempty >>=
-              loadAndApplyTemplate "templates/index-content.html" ctx >>=
-              saveSnapshot "page" >>=
-              loadAndApplyTemplate "templates/index.html" ctx >>=
-              finish ctx
+    compile $ let
+      pageCtx = paginateContext pages n
+      ctx =
+        constField "title" ("Page " <> show n) <>
+        listField "posts"
+                  (teaserField "teaser" "teaser" <>
+                   tagsCtx tags <>
+                   pageCtx <>
+                   postCtx)
+                  (recentFirst =<< loadAll pat) <>
+        pageCtx <>
+        defaultContext in
+      makeItem mempty >>=
+      loadAndApplyTemplate "templates/index-content.html" ctx >>=
+      saveSnapshot "page" >>=
+      loadAndApplyTemplate "templates/index.html" ctx >>=
+      finish ctx
   create ["index.html"] $ do
     route idRoute
-    compile $ let ctx = constField "home-page" mempty <>
-                        constField "title" "Home" <>
-                        defaultContext in
-              loadSnapshotBody (lastPage pages) "page" >>= makeItem >>=
-              loadAndApplyTemplate "templates/index.html" ctx >>=
-              finish ctx
+    compile $ let
+      ctx =
+        constField "home-page" mempty <>
+        constField "title" "Home" <>
+        defaultContext in
+      loadSnapshotBody (lastPage pages) "page" >>= makeItem >>=
+      loadAndApplyTemplate "templates/index.html" ctx >>=
+      finish ctx
 
   match "posts/*/overlay.md" $ compile pandocCompiler
   match "posts/*/warnings.md" $ compile getResourceBody
   match postPat $ do
-      route . customRoute $ \ident -> "posts/" <>
-                                     (takeBaseName . takeDirectory . toFilePath) ident <>
-                                     "/index.html"
-      compile $ let ctx = warnCtx <> overCtx <> jsCtx <> cssCtx <> tagsCtx tags <> postCtx in
-        do bib <- load "misc/biblio.bib"
-           csl <- load "misc/biblio.csl"
-           getResourceBody >>=
-             readPandocBiblio readerOpt csl bib >>=
-             saveSnapshot "teaser" . (demoteHeaders . demoteHeaders <$>) . writePandocWith writerOpt >>=
-             loadAndApplyTemplate "templates/post.html" ctx >>=
-             finish ctx
+      route . customRoute $ \ident ->
+        "posts/" <>
+        (takeBaseName . takeDirectory . toFilePath) ident <>
+        "/index.html"
+      compile $ let
+        ctx = warnCtx <> overCtx <> jsCtx <>
+              cssCtx <> tagsCtx tags <> postCtx in do
+        bib <- load "misc/biblio.bib"
+        csl <- load "misc/biblio.csl"
+        getResourceBody >>=
+          readPandocBiblio readerOpt csl bib >>=
+          saveSnapshot "teaser" . (demoteHeaders . demoteHeaders <$>) .
+            writePandocWith writerOpt >>=
+          loadAndApplyTemplate "templates/post.html" ctx >>=
+          finish ctx
 
   create ["archive"] $ do
       route $ constRoute "posts/index.html"
-      compile $ let ctx = constField "title" "Archive" <>
-                          constField "archive-page" mempty <>
-                          listField "posts"
-                                    (tagsCtx tags <> postCtx)
-                                    (recentFirst =<< loadAll postPat) <>
-                          defaultContext in
-                makeItem mempty >>=
-                loadAndApplyTemplate "templates/archive.html" ctx >>=
-                finish ctx
+      compile $ let
+        ctx = constField "title" "Archive" <>
+              constField "archive-page" mempty <>
+              listField "posts"
+                        (tagsCtx tags <> postCtx)
+                        (recentFirst =<< loadAll postPat) <>
+                        defaultContext in
+        makeItem mempty >>=
+        loadAndApplyTemplate "templates/archive.html" ctx >>=
+        finish ctx
 
   match "misc/biblio.csl" $ compile cslCompiler
   match "misc/biblio.bib" $ compile biblioCompiler
@@ -98,17 +111,21 @@ main = hakyll $ do
   match "images/**" $ do
       route idRoute
       compile copyFileCompiler
-  match "css/default/*.css" $ compile getResourceBody
-  create ["css/default.css"] $ do
-    route idRoute
-    compile $ loadAll "css/default/*.css" >>= makeItem . compressCss . concatMap itemBody
-  match "css/*.css" $ do
-    route idRoute
-    compile copyFileCompiler
+  match "css/default/site.scss" $ do
+    route $ constRoute "css/default.css"
+    compile $ do
+      i <- getResourceString
+      let dir = takeDirectory . toFilePath . itemIdentifier $ i
+      withItemBody (unixFilter "scss"
+                               ["--sourcemap=none", "--trace", "-I", dir]) i >>=
+        return . fmap compressCss
   match "js/default/*.js" $ compile getResourceBody
   create ["js/default.js"] $ do
     route idRoute
-    compile $ loadAll "js/default/*.js" >>= makeItem . compressJs . concatMap itemBody
+    compile $
+      loadAll "js/default/*.js" >>=
+      compressJs . concatMap itemBody >>=
+      makeItem
   match "js/*.js" $ do
     route idRoute
     compile copyFileCompiler
@@ -117,17 +134,17 @@ postPat :: Pattern
 postPat = "posts/*/main.md"
 
 finish :: Context String -> Item String -> Compiler (Item String)
-finish ctx i = loadAndApplyTemplate "templates/default.html" ctx i >>=
-           ((replaceAll "index.html" (const mempty) <$>) <$>) . relativizeUrls
+finish ctx i =
+  loadAndApplyTemplate "templates/default.html" ctx i >>=
+  ((replaceAll "index.html" (const mempty) <$>) <$>) . relativizeUrls
 
---TODO: Actual js compressor
-compressJs :: String -> String
-compressJs = id
-
+compressJs :: String -> Compiler String
+compressJs = unixFilter "uglifyjs" ["-cm", "--screw-ie8"]
 postCtx :: Context String
-postCtx = dateField "date" "%B %e, %Y" <>
-          dateField "num-date" "%F" <>
-          defaultContext
+postCtx =
+  dateField "date" "%B %e, %Y" <>
+  dateField "num-date" "%F" <>
+  defaultContext
 
 paginateOverflow :: Int -> [a] -> [[a]]
 paginateOverflow low xs =
@@ -137,22 +154,27 @@ paginateOverflow low xs =
     pgs = paginateEvery low xs
 
 lastPage :: Paginate -> Identifier
-lastPage pg = paginateMakeId pg . M.size . paginateMap $ pg where
+lastPage pg = paginateMakeId pg . M.size . paginateMap $ pg
 
-listFieldWith' :: String -> Context a -> (Identifier -> Compiler [a]) -> Context b
-listFieldWith' k ctx f = listFieldWith k ctx $ (mapM makeItem =<<) . f . itemIdentifier
+listFieldWith' :: String -> Context a -> (Identifier -> Compiler [a]) ->
+                  Context b
+listFieldWith' k ctx f =
+  listFieldWith k ctx $ (mapM makeItem =<<) . f . itemIdentifier
 
 overCtx :: Context String
-overCtx = field "overlay" $ loadBody . fromFilePath . (<> "/overlay.md") . takeDirectory . toFilePath . itemIdentifier
+overCtx =
+  field "overlay" $ loadBody . fromFilePath . (<> "/overlay.md") .
+  takeDirectory . toFilePath . itemIdentifier
 warnCtx :: Context String
-warnCtx = listFieldWith "warnings"
-                        (details <> summary)
-                        ((mapM makeItem =<<) . (lines <$>) . loadBody .
-                         fromFilePath . (<> "/warnings.md") .
-                         takeDirectory . toFilePath . itemIdentifier) where
-  split g = return . g . break (== '|') . itemBody
-  details = field "details" $ split (tail . snd)
-  summary = field "summary" $ split fst
+warnCtx =
+  listFieldWith "warnings"
+                (details <> summary)
+                ((mapM makeItem =<<) . (lines <$>) . loadBody .
+                 fromFilePath . (<> "/warnings.md") .
+                 takeDirectory . toFilePath . itemIdentifier) where
+    split g = return . g . break (== '|') . itemBody
+    details = field "details" $ split (tail . snd)
+    summary = field "summary" $ split fst
 
 jsCtx :: Context String
 jsCtx = listFieldWith' "jses" fileNameCtx $ getListMeta "js"
@@ -161,25 +183,32 @@ cssCtx = listFieldWith' "csses" fileNameCtx $ getListMeta "css"
 fileNameCtx :: Context String
 fileNameCtx = field "filename" $ return . itemBody
 getListMeta :: MonadMetadata m => String -> Identifier -> m [String]
-getListMeta k ident = return . maybe [] (map trim . splitAll ",") . M.lookup k =<<
-                      getMetadata ident
+getListMeta k ident =
+  return . maybe [] (map trim . splitAll ",") . M.lookup k =<< getMetadata ident
 
 tagsCtx :: Tags -> Context String
-tagsCtx tags = listFieldWith' "tags" tagCtx getTags where
-  tagCtx = (field "tag-name" $ return . itemBody) <>
-           (field "tag-url" $ return . toUrl . toFilePath . (tagsMakeId tags) . itemBody)
+tagsCtx tags =
+  listFieldWith' "tags" tagCtx getTags where
+    tagCtx = (field "tag-name" $ return . itemBody) <>
+             (field "tag-url" $ return . toUrl . toFilePath .
+              (tagsMakeId tags) . itemBody)
 
 readerOpt :: ReaderOptions
-readerOpt = defaultHakyllReaderOptions { readerExtensions = S.insert Ext_compact_definition_lists $ readerExtensions def }
+readerOpt =
+  defaultHakyllReaderOptions {
+    readerExtensions = S.insert Ext_compact_definition_lists $ readerExtensions def
+    }
 
 writerOpt :: WriterOptions
-writerOpt = defaultHakyllWriterOptions { writerHtml5 = True
-                                       , writerHtmlQTags = True
-                                       , writerHTMLMathMethod = MathJax ""
-                                       -- , writerTableOfContents = True
-                                       -- , writerStandalone = True
-                                       -- , writerTemplate = unlines ["$toc$", "$body$"]
-                                       }
+writerOpt =
+  defaultHakyllWriterOptions {
+    writerHtml5 = True,
+    writerHtmlQTags = True,
+    writerHTMLMathMethod = MathJax ""
+    -- writerTableOfContents = True
+    -- writerStandalone = True
+    -- writerTemplate = unlines ["$toc$", "$body$"]
+    }
 
 perPage :: Int
 perPage = 5
