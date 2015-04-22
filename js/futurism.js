57fe99447047335dec9cd9d702d1f5aa6f427601
Cole Haus <colehaus@cryptolab.net>
2015-04-21 23:31:20 -0400

    Bump

--- js/futurism.js
+++ js/futurism.js
@@ -49,7 +49,7 @@
           return h / 2;
         }
       }, moreLikely: {
-        label: ['Believing the', 'scenario more likely'],
+        label: ['Believe the', 'scenario more likely'],
         type: 'outcome',
         x: function x(w) {
           return w / 4;
@@ -132,11 +132,11 @@
       }
     };
 
-    var linkData = [{ source: 'mediocre', target: 'other', type: 'motivates' }, { source: 'mediocre', target: 'multiple', type: 'motivates' }, { source: 'optimistic', target: 'other', type: 'motivates' }, { source: 'optimistic', target: 'multiple', type: 'motivates' }, { source: 'overconfident', target: 'other', type: 'motivates' }, { source: 'overconfident', target: 'multiple', type: 'motivates' }, { source: 'single', target: 'moreLikely', type: 'causes' }, { source: 'naive', target: 'single', type: 'subtype' }, { source: 'naive', target: 'overconfident', type: 'causes' }, { source: 'naive', target: 'mediocre', type: 'causes' }, { source: 'naive', target: 'optimistic', type: 'causes' }, { source: 'multiple', target: 'outcomes', type: 'causes' }, { source: 'multiple', target: 'widened', type: 'causes' }, { source: 'multiple', target: 'narrowed', type: 'causes' }, { source: 'multiple', target: 'framing', type: 'causes' }, { source: 'other', target: 'framing', type: 'causes' }, { source: 'pessimistic', target: 'multiple', type: 'contradicts' }, { source: 'multiple', target: 'rational', type: 'causes' }];
+    var linkData = [{ source: 'mediocre', target: 'other', type: 'motivates' }, { source: 'mediocre', target: 'multiple', type: 'motivates' }, { source: 'optimistic', target: 'other', type: 'motivates' }, { source: 'optimistic', target: 'multiple', type: 'motivates' }, { source: 'overconfident', target: 'other', type: 'motivates' }, { source: 'overconfident', target: 'multiple', type: 'motivates' }, { source: 'naive', target: 'single', type: 'supertype' }, { source: 'pessimistic', target: 'multiple', type: 'contradicts' }, { target: 'single', source: 'moreLikely', type: 'describes' }, { target: 'naive', source: 'overconfident', type: 'describes' }, { target: 'naive', source: 'mediocre', type: 'describes' }, { target: 'naive', source: 'optimistic', type: 'describes' }, { target: 'multiple', source: 'outcomes', type: 'describes' }, { target: 'multiple', source: 'widened', type: 'describes' }, { target: 'multiple', source: 'narrowed', type: 'describes' }, { target: 'multiple', source: 'framing', type: 'describes' }, { target: 'other', source: 'framing', type: 'describes' }, { target: 'multiple', source: 'rational', type: 'describes' }];
 
     var nodeTypeData = [{ type: 'outcome', label: ['Outcome'], shape: argMap.shapes.circle }, { type: 'method', label: ['Prediction method'], shape: argMap.shapes.square }, { type: 'conclusion', label: ['Conclusion'], shape: argMap.shapes.diamond }];
 
-    var linkTypeData = [{ type: 'causes', label: ['Possible causation'] }, { type: 'subtype', label: ['Subtype'] }, { type: 'contradicts', label: ['Contradict'] }, { type: 'motivates', label: ['Motivate'] }];
+    var linkTypeData = [{ type: 'describes', label: ['Describes'] }, { type: 'supertype', label: ['Supertype'] }, { type: 'contradicts', label: ['Contradict'] }, { type: 'motivates', label: ['Motivate'] }];
 
     argMap.handler(argMap.mkMap('#arg-map', nodeData, linkData, nodeTypeData, linkTypeData));
   });
