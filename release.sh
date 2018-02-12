fc17aa72028948487c86fcf80f2b33ea21b0b13e
Cole Haus <colehaus@cryptolab.net>
2018-02-12 07:32:18 -0800

    Don't verify on release

--- release.sh
+++ release.sh
@@ -3,15 +3,15 @@
 set -euxo pipefail
 
 git checkout master
-revision=`git log -n 1 --pretty=format:"%H"`
-comment=`git log -n 1 --pretty=format:"%B"`
+revision=`git log --max-count=1 --pretty=format:"%H"`
+comment=`git log --max-count=1 --pretty=format:"%B"`
 access_token=`cat ./rollbar-access-token.txt`
 environment=production
-nix-build -o colex-result
+nix-build --out-link colex-result
 git checkout gh-pages
 sudo cp -r colex-result/* .
-git add -i
-git diff-index --quiet HEAD || git commit -m "Bump"
+git add --interactive
+git diff-index --quiet HEAD || git commit --no-verify --message "Bump"
 git push
 curl https://api.rollbar.com/api/1/deploy/ \
      -F access_token=$access_token \
