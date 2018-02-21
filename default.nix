bcf158499d87ccaadbae62d289f598d13c8b8930
Cole Haus <colehaus@cryptolab.net>
2018-02-20 18:09:10 -0800

    Fix nix to use required versions of Haskell dependencies
    We only work with the most recent versions of pandoc. In previous commits, we cheated and relied on pandoc. Now we've nixified the fix.
    
    Also, pinned our nixpkgs for reproducible builds.

--- default.nix
+++ default.nix
@@ -1,2 +1,23 @@
-{ pkgs ? import <nixpkgs> {} } :
-  pkgs.callPackage ./hakyllColEx.nix {}
+let
+  bootstrap = import <nixpkgs> {};
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+  pkgs = import src {
+    config = {
+      packageOverrides = pkgs: rec {
+        haskellPackages = pkgs.haskellPackages.override {
+          overrides = haskellPackagesNew: haskellPackagesOld: rec {
+            pandoc = haskellPackagesOld.pandoc_2_1_1;
+            hakyll = pkgs.haskell.lib.dontCheck haskellPackagesOld.hakyll_4_11_0_0;
+            pandoc-citeproc = haskellPackagesNew.callHackage "pandoc-citeproc" "0.13" {};
+          };
+        };
+      };
+    };
+  };
+in
+pkgs.callPackage ./hakyllColEx.nix {}
