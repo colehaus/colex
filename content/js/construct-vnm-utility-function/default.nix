960d47f542747d733aa9b465b5ff2b5d17e88b2b
Cole Haus <colehaus@cryptolab.net>
2018-05-07 20:42:46 -0700

    Switch to updated nix-gitignore
    Now handles trailing slashes

--- content/js/construct-vnm-utility-function/default.nix
+++ content/js/construct-vnm-utility-function/default.nix
@@ -1,7 +1,8 @@
 { pkgs ? import <nixpkgs> {}, extras ? import ../../../extras.nix } :
   extras.callPurescript2nix {
     inherit pkgs;
-    name = "value-of-information-calculator";
-    src = ./.;
+    name = "construct-vnm-utility-function";
+    src = extras.gitignoreSource ../../.. + "/content/js/construct-vnm-utility-function";
     executable = true;
+    npm = true;
   }
--- content/js/value-of-information-calculator/default.nix
+++ content/js/value-of-information-calculator/default.nix
@@ -2,6 +2,6 @@
   extras.callPurescript2nix {
     inherit pkgs;
     name = "value-of-information-calculator";
-    src = ./.;
+    src = extras.gitignoreSource ../../.. + "/content/js/value-of-information-calculator";
     executable = true;
   }
