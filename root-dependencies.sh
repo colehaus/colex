d1216a2e2fc50db07969e6b38647643791f06a22
Cole Haus <colehaus@cryptolab.net>
2019-06-25 18:19:47 -0700

    Clean old gc roots before adding new ones

--- root-dependencies.sh
+++ root-dependencies.sh
@@ -3,5 +3,7 @@
 set -euxo pipefail
 
 cd nix
+rm no-gc-outs-*
+rm no-gc-drvs-*
 nix-instantiate ../default.nix -A noGcDeps --indirect --add-root no-gc-drvs
 nix-store --indirect --add-root no-gc-outs --realise $(nix-instantiate ../default.nix -A noGcDeps)
